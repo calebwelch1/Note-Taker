c8becb90451d4d7ae5aaa6299d68d0568a302712
calebwelch <caleb.welch11@gmail.com>
2020-06-10 16:52:46 -0400

    initial server

--- Note-Taker/Develop/public/assets/js/index.js
+++ Note-Taker/Develop/public/assets/js/index.js
@@ -149,3 +149,19 @@ $noteText.on("keyup", handleRenderSaveBtn);
 
 // Gets and renders the initial list of notes
 getAndRenderNotes();
+
+// make routes
+express = require("express");
+path = require("path");
+app = express();
+const PORT = 8080;
+
+app;
+// GET `/notes` - Should return the `notes.html` file.
+app.get("/notes.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "assets"));
+});
+// GET `*` - Should return the `index.html` file
+
+// listener
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
