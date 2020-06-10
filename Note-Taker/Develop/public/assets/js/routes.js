a22459965d8a9c955dd22425aa60c20093ad9fa2
calebwelch <caleb.welch11@gmail.com>
2020-06-10 18:43:09 -0400

    index route, fs

--- Note-Taker/Develop/public/assets/js/routes.js
+++ Note-Taker/Develop/public/assets/js/routes.js
@@ -2,6 +2,7 @@
 express = require("express");
 path = require("path");
 app = express();
+fs = require("fs");
 const PORT = 8080;
 
 // make static folder
@@ -13,6 +14,8 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "..", "notes.html"));
 });
 // GET `*` - Should return the `index.html` file
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "..", "index.html"));
+});
 // listener
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
